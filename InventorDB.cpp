258a1405810869c08c5b645d75c802f3cc9d99cc
DERRICKDRANGO <>
2022-05-23 16:16:36 +0000

    Done

--- InventorDB.cpp
+++ InventorDB.cpp
@@ -9,6 +9,9 @@ using namespace std;
 Set head and tail to nullptr
 */
 InventorDB::InventorDB(){
+tail = nullptr;
+head = nullptr;
+
 }
 
 InventorDB::~InventorDB() {
@@ -16,32 +19,77 @@ InventorDB::~InventorDB() {
 }
 
 // Delete all the current nodes and set head & tail to nullptr
-void InventorDB::deleteAllNodes() {
+ void InventorDB::deleteAllNodes() {
+while(head != nullptr) {
+	Node *next = head->next;
+	delete head;
+	head = next;
+}
+tail = nullptr;
 }
 
 // Append a new entry at the tail of the list
 void InventorDB::append(Inventor inventor) {
+  Node *nodePtr = new Node(inventor);
+    if (tail == nullptr) {
+    head = nodePtr;
+}
+else {
+     tail->next = nodePtr;
+}
+tail = nodePtr;
 }
 
 // Traverse the list printing all the data in the nodes
 void InventorDB::print() {
+  Node *curr = head;
+while(curr != nullptr) {
+    curr->data.print();
+    curr = curr->next;
+}
 }
 
 // Insert a new entry at the head of the list
 void InventorDB::insertInFront(Inventor inventor){
+       Node *nodePtr = new Node(inventor);
+  
+    if (tail == nullptr) {
+    head = nodePtr;
+      }
+      
+else {
+  
+     nodePtr->next = head;
+  
+  }
+head = nodePtr;
 }
 
 // If there is an entry at the head of the list, remove it.
 void InventorDB::removeFront() {
+ if (head != nullptr) {
+	Node *next = head->next;
+   
+	delete head;
+	head = next;
 }
-
-
+if (head == nullptr) {
+tail = nullptr;
+}    
+  }
 // Traverse the list and if a call to isAlive on the data
 // in the node returns true, print that data.
 void InventorDB::printAlive() {
+ Node *curr = head;
+  
+while(curr != nullptr) {
+  if (curr->data.isAlive() == true) {
+    curr->data.print();
+    curr = curr->next;
+  }
+  
 }
-
-
+}
 
 /**
 Print a menu with the things the user is allowed to do with this database.
@@ -75,6 +123,9 @@ void InventorDB::dbMenu(){
     // Spin here until the user enters the QUIT value.
     int option = -1;
     Inventor temp;
+
+
+  
     while(option != QUIT) {
         // Print the menu options (we use index - 1) because 
         // the first option is 1, but the first entry in the array
